3226e9562bca82612e630eb40ccd407b2f1ac633
YOSHIOKA Eiichiro <yoshioka.eiichiro@gmail.com>
2014-05-27 23:13:56 +0900

    first commit

---
+++ wiifit/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace wiifit
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.cmbSex = new System.Windows.Forms.ComboBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.lblHeighｔ = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblCondition = new System.Windows.Forms.Label();
+            this.cmbCondition = new System.Windows.Forms.ComboBox();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.cmbPosition = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 240);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(422, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(609, 218);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "測定開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(609, 247);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(75, 23);
+            this.btnFinish.TabIndex = 5;
+            this.btnFinish.Text = "測定終了";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(475, 26);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 19);
+            this.txtID.TabIndex = 6;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(453, 29);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(16, 12);
+            this.lblID.TabIndex = 7;
+            this.lblID.Text = "ID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(440, 54);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(29, 12);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "名前";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(475, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 19);
+            this.txtName.TabIndex = 8;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(440, 79);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 12);
+            this.lblAge.TabIndex = 11;
+            this.lblAge.Text = "年齢";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(475, 76);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(34, 19);
+            this.txtAge.TabIndex = 10;
+            // 
+            // cmbSex
+            // 
+            this.cmbSex.FormattingEnabled = true;
+            this.cmbSex.Items.AddRange(new object[] {
+            "男性",
+            "女性"});
+            this.cmbSex.Location = new System.Drawing.Point(475, 102);
+            this.cmbSex.Name = "cmbSex";
+            this.cmbSex.Size = new System.Drawing.Size(121, 20);
+            this.cmbSex.TabIndex = 12;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(440, 105);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(29, 12);
+            this.lblSex.TabIndex = 13;
+            this.lblSex.Text = "性別";
+            // 
+            // lblHeighｔ
+            // 
+            this.lblHeighｔ.AutoSize = true;
+            this.lblHeighｔ.Location = new System.Drawing.Point(440, 131);
+            this.lblHeighｔ.Name = "lblHeighｔ";
+            this.lblHeighｔ.Size = new System.Drawing.Size(29, 12);
+            this.lblHeighｔ.TabIndex = 15;
+            this.lblHeighｔ.Text = "身長";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(475, 128);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(34, 19);
+            this.txtHeight.TabIndex = 14;
+            // 
+            // lblCondition
+            // 
+            this.lblCondition.AutoSize = true;
+            this.lblCondition.Location = new System.Drawing.Point(440, 156);
+            this.lblCondition.Name = "lblCondition";
+            this.lblCondition.Size = new System.Drawing.Size(29, 12);
+            this.lblCondition.TabIndex = 17;
+            this.lblCondition.Text = "条件";
+            // 
+            // cmbCondition
+            // 
+            this.cmbCondition.FormattingEnabled = true;
+            this.cmbCondition.Items.AddRange(new object[] {
+            "開眼",
+            "閉眼"});
+            this.cmbCondition.Location = new System.Drawing.Point(475, 153);
+            this.cmbCondition.Name = "cmbCondition";
+            this.cmbCondition.Size = new System.Drawing.Size(121, 20);
+            this.cmbCondition.TabIndex = 16;
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(440, 182);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(29, 12);
+            this.lblPosition.TabIndex = 19;
+            this.lblPosition.Text = "肢位";
+            // 
+            // cmbPosition
+            // 
+            this.cmbPosition.FormattingEnabled = true;
+            this.cmbPosition.Items.AddRange(new object[] {
+            "開眼",
+            "閉眼"});
+            this.cmbPosition.Location = new System.Drawing.Point(475, 179);
+            this.cmbPosition.Name = "cmbPosition";
+            this.cmbPosition.Size = new System.Drawing.Size(121, 20);
+            this.cmbPosition.TabIndex = 18;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 281);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.cmbPosition);
+            this.Controls.Add(this.lblCondition);
+            this.Controls.Add(this.cmbCondition);
+            this.Controls.Add(this.lblHeighｔ);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.cmbSex);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmMain";
+            this.Text = "重心動揺測定";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox cmbSex;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.Label lblHeighｔ;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblCondition;
+        private System.Windows.Forms.ComboBox cmbCondition;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.ComboBox cmbPosition;
+    }
+}
+
---
+++ wiifit/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using WiimoteLib;
+
+namespace wiifit
+{
+    public partial class frmMain : Form
+    {
+        Wiimote wm = new Wiimote();
+        StreamWriter sw = new StreamWriter(@"C:\Test.csv", true, Encoding.GetEncoding("Shift_JIS"));
+        string output_result;
+        public frmMain()
+        {
+            InitializeComponent();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.wm.Connect();
+            //各種メタデータを格納
+            output_result = txtID.Text + "," + txtName.Text + "," + txtAge.Text + "," + cmbSex.Text + "," + txtHeight.Text + "," + cmbCondition.Text + "," + cmbPosition.Text;
+            this.wm.WiimoteChanged += wm_WiimoteChanged;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.wm.Disconnect();
+            sw.WriteLine(output_result);
+            sw.Close();
+        }
+
+        void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs args)
+        {
+            //WiimoteStateの値を取得
+            WiimoteState ws = args.WiimoteState;
+
+            //ピクチャーボックスへ描画
+            this.DrawForms(ws);
+
+            //ラベル
+
+            //重さ(Kg)表示
+            this.label1.Text = ws.BalanceBoardState.WeightKg + "kg";
+            //重心のX座標表示
+            this.label2.Text = "X:" +
+                ((int)ws.BalanceBoardState.CenterOfGravity.X * (43.2 / 43)).ToString();
+            //重心のY座標表示
+            this.label3.Text = "Y:" +
+                ((int)ws.BalanceBoardState.CenterOfGravity.Y * (23.7 / 24)).ToString();
+            //結果出力用変数に格納
+            output_result += ((int)ws.BalanceBoardState.CenterOfGravity.X * (43.2 / 43)).ToString() + "," + ((int)ws.BalanceBoardState.CenterOfGravity.Y * (23.7 / 24)).ToString() + "," + ws.BalanceBoardState.WeightKg + Environment.NewLine;
+        }
+        //フォーム描写関数
+        public void DrawForms(WiimoteState ws)
+        {
+            //pictureBox1のグラフィックスを取得
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.Clear(Color.Black);     //画面を黒色にクリア
+
+            //X、Y座標の計算
+            float x =
+                (wm.WiimoteState.BalanceBoardState.CenterOfGravity.X
+                + 20.0f) * 10;    //表示位置(X座標)を求める
+            float y =
+                (wm.WiimoteState.BalanceBoardState.CenterOfGravity.Y
+                + 12.0f) * 10;    //表示位置(Y座標)を求める
+
+            //赤色でマーカを描写
+            g.FillEllipse(Brushes.Red, x, y, 10, 10);
+
+            g.Dispose();  //グラフィックスを開放
+        }
+    }
+}
